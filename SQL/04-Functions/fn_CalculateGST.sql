06e28918a168487f50f0dca7b1ecea02c635f662
Parkar_Hrushikesh <phrushi98@gmail.com>
2026-07-05 21:47:15 +0530

    Update fn_CalculateGST.sql

--- SQL/04-Functions/fn_CalculateGST.sql
+++ SQL/04-Functions/fn_CalculateGST.sql
@@ -1,3 +1,13 @@
+/*
+=========================================
+Object Name : dbo.fn_CalculateGST
+Object Type : Scalar Function
+Author      : Hrushikesh Parkar
+Created On  : 05-Jul-2026
+Project     : Enterprise DevOps Portfolio
+Description : Calculates 18% GST for a given price.
+=========================================
+*/
 CREATE OR ALTER FUNCTION dbo.fn_CalculateGST
 (
     @Price DECIMAL(10,2)
